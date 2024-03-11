ce7c17bb5d12f4737fdf3e3eedddef065b2a3595
DONAT: Developers of Next Advanced Technology <152084590+DonatOrca@users.noreply.github.com>
2024-03-11 16:08:57 +0800

    add: router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 
 function App() {
   return (
@@ -16,6 +15,11 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      <nav>
+        <Link to={"/"}>Dashboard</Link>
+        {" | "}
+        <Link to={"/announcement"}>Announcement</Link>
+      </nav>
       <Outlet />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more

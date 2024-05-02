b9c096a6ad17c2afcf9fecc99a7287e44e839a34
audrNa <audrna7@gmail.com>
2024-05-02 19:58:04 +0800

    Add WIP message to announcements page

--- src/pages/Announcements.tsx
+++ src/pages/Announcements.tsx
@@ -1,9 +1,21 @@
+import { Link } from "react-router-dom";
+
 function Announcements() {
   return (
     <>
-      <h1 className="text-center font-lovelo text-3xl text-[#AD4DD1]">
-        Announcements
-      </h1>
+      <h1 className="text-center font-lovelo text-3xl">Announcements</h1>
+      <br />
+      <p className="text-center">
+        This page is currently a work-in-progress, please refer to our{" "}
+        <Link
+          to="https://www.facebook.com/OrCaDONAT"
+          target="_blank"
+          className="underline"
+        >
+          facebook page
+        </Link>{" "}
+        instead. Thank you! 🍩
+      </p>
     </>
   );
 }

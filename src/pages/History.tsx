892240679edda3ba790227944d4f85c6fcf0b86b
audrNa <audrna7@gmail.com>
2024-08-29 19:54:55 +0800

    Update history page

--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -5,12 +5,20 @@ interface ChildrenProps {
   children: ReactNode;
 }
 
+interface ImageProps {
+  src: string;
+}
+
 /**
  * Use this variable as values for header texts
  * I initially tried to automate this better but I hate React
  * and I don't have time to learn hooks contexts and shit
  */
-const HEADERS: string[] = ["The History of DONAT", "Sparkle", "Stories"];
+const HEADERS: string[] = [
+  "The History of DONAT",
+  "Sir Bes",
+  "DONAT-Bytes (2016-2017)",
+];
 
 function History() {
   return (
@@ -31,116 +39,117 @@ function Contents() {
     "
     >
       <H>{HEADERS[0]}</H>
+      <H>from Sir Leandro's Eyes</H>
       <P>
-        &gt; 🚧 Apologies, but this page is currently a work-in-progress. Now
-        don't mind me just testing some long paragraphs and headers. 🚧 &lt;
-      </P>
-      <P>
-        Lorem Ipsum is simply dummy text of the printing and typesetting
-        industry. Lorem Ipsum has been the industry's standard dummy text ever
-        since the 1500s, when an unknown printer took a galley of type and
-        scrambled it to make a type specimen book. It has survived not only five
-        centuries, but also the leap into electronic typesetting, remaining
-        essentially unchanged. It was popularised in the 1960s with the release
-        of Letraset sheets containing Lorem Ipsum passages, and more recently
-        with desktop publishing software like Aldus PageMaker including versions
-        of Lorem Ipsum.
+        DONAT was formed during 2016-2017 when we were grade 11 students. As far
+        as I know, it was formed in order to help the MAWD students (Formerly
+        MAWP) with their researches and their overall holistic development and
+        academics. The very first adviser of DONAT was sir Aries Jayvee Ganzon,
+        who now works as the lead data analyst of Johnson and Johnson.
       </P>
 
       <P>
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It
-        has roots in a piece of classical Latin literature from 45 BC, making it
-        over 2000 years old. Richard McClintock, a Latin professor at
-        Hampden-Sydney College in Virginia, looked up one of the more obscure
-        Latin words, consectetur, from a Lorem Ipsum passage, and going through
-        the cites of the word in classical literature, discovered the
-        undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33
-        of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by
-        Cicero, written in 45 BC. This book is a treatise on the theory of
-        ethics, very popular during the Renaissance. The first line of Lorem
-        Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section
-        1.10.32.
+        Along with the other ICT Faculty teachers during that time, they were
+        providing us with multiple different activities that will help guide and
+        serve as a way to form and create an organization that will develop
+        students into future student leaders as well as proper industry
+        professionals.
+      </P>
+
+      <P>
+        The very first president of DONAT came from my section when we were
+        students, his name was John Nelzon Chan, He now works for STI's head
+        office as one of the people who creates the pubmats for the whole STI
+        network to see. The VP during that time was Era Mae Rutaquio, also one
+        of my classmates and the girlfriend of the President. She also now works
+        alongside Nelzon for STI's Head office.
+      </P>
+
+      <P>
+        The first auditor was my classmate as well, Leomhel Atienza. The
+        Secretary, Blessy Sibalon also used to be my classmate. Most, if not,
+        all of the officers of DONAT during the first year came from my section.
+        We were regarded as the top performing section during our time as most
+        of the student leaders as well as top performing students came from our
+        section as well.
       </P>
 
       <H>{HEADERS[1]}</H>
       <P>
-        Skill: Increases the CRIT DMG of a single ally by 12%—26.4% of Sparkle's
-        CRIT DMG plus 27%—48.6%, lasting for 1 turn(s). And at the same time,
-        Advances Forward this ally's action by 50%. When Sparkle uses this
-        ability on herself, the Action Advance effect will not trigger.
-        Ultimate: Recovers 4 Skill Points for the team and grants all allies
-        Cipher. When allies with Cipher trigger the DMG Boost effect provided by
-        Sparkle's Talent, each stack additionally increases its effect by
-        6%—10.8%, lasting for 2 turns. Talent: While Sparkle is on the
-        battlefield, additionally increases the max number of Skill Points by 2.
-        Whenever an ally consumes 1 Skill Point, all allies' DMG increases by
-        3%—6.6%. This effect lasts for 2 turn(s) and can stack up to 3 time(s).
-        Technique: Using the Technique grants all allies Misdirect for 20
-        seconds. Characters with Misdirect will not be detected by enemies, and
-        entering battle in the Misdirect state recovers 3 Skill Point(s) for the
-        team. Almanac: When using Basic ATK, additionally regenerates 10 Energy.
-        Artificial Flower: The CRIT DMG Boost effect provided by the Skill will
-        extend to last until the start of the target's next turn. Nocturne :
-        Increases all allies' ATK by 15%. When there are 1/2/3 Quantum allies in
-        your team, increases Quantum-Type allies' ATK by 5%/15%/30%.
+        During the conception of DONAT, there came a time where grade 12
+        students were introduced to the subject entitled Triple I and Practical
+        Research. As the pioneers of K-12, the grade 12 students of our time
+        were the first to experience a "new curriculum", therefore, most of the
+        contents of the research-type subjects were very new and used to be
+        templated from the college curriculum.
       </P>
 
       <P>
-        Sparkle is a petite young woman who has black hair that is tied into two
-        high pigtails with a red tie. Her hair reaches her hips and has pink
-        tips, fringe middle bangs, and two locks in front, with the left lock
-        reaching her collarbone and the right lock reaches her shoulder. She has
-        fluorescent pink eyes with a light colored butterfly detail under her
-        pupils, a small cherry blossom flower tattoo on her right collarbone,
-        and tiny black dots under each of her eyes. Additionally, a
-        white-and-red Kitsune mask is at an angle on the top of her head, which
-        has a pink flower gem on its forehead. Below it, a bell is tied into her
-        hair, with another bell hanging off a black neck piece.
-      </P>
-      <P>
-        Sparkle wears a sleeveless and backless short red kimono with crossing
-        black spaghetti straps. Cherry blossom flowers and a grey trim decorate
-        the kimono along with a black obi belt overlaid with a small golden
-        rope, which is tied into a bow. Attached to it is a white spherical
-        ornament with a koi fish inside, with there being a golden tassel
-        attached under it. On the back of her kimono is a large golden rope bow
-        from the back, under which is a black large tied bow. She also wears
-        detached short red sleeves with hanging fabric that reaches her knees,
-        along with a cherry blossom flower ornament and a tassel hanging from
-        it. Two light salmon colored fabrics hang from the back of her kimono,
-        reaching her calves, resembling caudal tails. She has a black glove on
-        her left hand, while her right hand is adorned with red ropes with a
-        small bell attached to it. On her feet, she wears black sandals with
-        thin red ankle straps, with small flowers embellishing each and a bell
-        hanging off the straps of the left sandal. Additionally, red nail polish
-        adorns her fingers and toes.
+        Sir AJ Ganzon, always wanted his students to become the BEST, and due to
+        the fact that sir AJ always claimed that he wanted the students to
+        always do their best and perform to the best of their abilities, he was
+        given the nickname "SIR BES". He was a very approachable teacher, he
+        often challenged us with coding practices as well as development
+        practices that were expected for college students. He also often used
+        words that would provoke us in a way that would affect us positively.
+        During the time that we were actively performing our research subjects,
+        he saw and noticed that most of the students were struggling with their
+        research. He then introduced a new system wherein the very first
+        collaboration of a Tertiary organization and a SHS organization was
+        formed.
       </P>
 
       <H>{HEADERS[2]}</H>
       <P>
-        The girl, an abandoned orphan, lived without knowledge of her origins or
-        destination until she encountered a passing troupe. From a distance, she
-        watched a black-haired lady with a double ponytail, gracefully moving
-        across the stage like a fish. Despite donning various masks, they did
-        not hinder her laughs and cries during her performance, captivating the
-        audience even from afar. The fish leaped in front of her, then dove back
-        down, creating ripples in the surface of the water.
+        The previous IT organization for tertiary students named "Bytes" had a
+        collaboration with DONAT and were called the "DONAT-Bytes". This
+        collaboration aims to provide the struggling Grade 12 students with
+        "Mentors". These mentors came from the tertiary students who willingly
+        and voluntarily chose to help the grade 12 students with their
+        researches.
       </P>
+
       <P>
-        Gradually, she realized she was watching the performance from beneath
-        the stage. Intrigued, she continued to watch the performances for
-        countless days, but she remained an audience member, never stepping into
-        the spotlight. After one of the shows ended, she went backstage, where
-        the black-haired lady handed her a mask. "Can I perform, too?" "Why not?
-        If others believe you are 'Sparkle,' then you are. Go, it's time for
-        your show."
+        Some of the notable mentors and members of DONAT-Bytes whom most of the
+        students are familiar with would be Sir Earlson Pasco, a current
+        Tertiary ICT Faculty and Ma'am Dianne Gongora, a previous DONAT Adviser.
       </P>
+
       <P>
-        As the curtain, serving as the fourth wall, was drawn, the stage
-        remained dimly lit, revealing an unsettling black void. Though wearing a
-        mask, she knew the audience could see and hear her. She kept recalling
-        Sparkle's performance, imitating her movements, voice, and demeanor.
+        Speaking from my personal experience, Sir AJ always used to tell me
+        things like "Is this the best you can do?". Even if I've poured all of
+        the effort and time that I had into creating a certain project and/or
+        system. He would always provoke me by saying those types of words. Yes,
+        sometimes it did hurt and it would dishearten me, however, I used the
+        frustration as a fuel to do better than what I currently am capable of.
+        This lead to me and my group being nominated and winning the "Best
+        lifestyle mobile application" of the very first SHS Expo.
       </P>
+
+      <P>
+        For reference, Here's the picture of the previous members of DONAT along
+        with Sir Bes in the middle. This is the very last GA of sir AJ as an
+        adviser as well as our very last GA as SHS students.
+      </P>
+
+      <Image src="history/donat_last_ga_2016.png" />
+
+      <P>
+        Here's another picture, some of the officers and members, from left to
+        right:
+        <List>
+          <li>Blessy Sibalon - Secretary</li>
+          <li>Nelzon Chan - President</li>
+          <li>Era Rutaquio - VP </li>
+          <li>Ako - wala lang trip lang</li>
+          <li>Denver Garcia - Member Dean</li>
+          <li>Chulvo - Member</li>
+          <li>Christine Marcelo - Treasurer </li>
+          <li>Norhaina Alon - Journalist/Socmed Manager</li>
+        </List>
+      </P>
+
+      <Image src="history/donat_officers_2016.png" />
     </div>
   );
 }
@@ -188,6 +197,14 @@ function simplify_header(header: string): string {
 /* A stylized paragraph element */
 function P({ children }: ChildrenProps) {
   return <p className="py-3 indent-12 font-kollektif text-lg">{children}</p>;
+}
+
+function Image({ src }: ImageProps) {
+  return <img className="rounded" src={src} />;
+}
+
+function List({ children }: ChildrenProps) {
+  return <ul className="py-3 indent-16 font-kollektif text-lg">{children}</ul>;
 }
 
 export default History;
